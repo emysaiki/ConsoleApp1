1c6f65e566673603696259086a6210029fc682b3
emysaiki <emysaiki@T20019314069123>
2022-04-04 10:17:45 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+double areaCircunferencia, raio, PI;
+PI = 3.1416;
+Console.WriteLine("Informe o raio da circunferencia");
+raio =Convert.ToDouble(Console.ReadLine());
+areaCircunferencia = PI * raio * raio;
+Console.WriteLine("A ÁREA DA CIRCUNFERENCIA É : " + areaCircunferencia);
